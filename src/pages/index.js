067679bc599d4702635e6d10b9cb8411adbab631
Mike Massie <mike@michaelmassie.com>
2020-04-10 07:35:16 -0500

    discoing content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    
     <p>Know what is available locally and in season</p>
     
     <button>Get location</button>
 
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
     <Link to="/page-2/">About</Link>
   </Layout>
 )
